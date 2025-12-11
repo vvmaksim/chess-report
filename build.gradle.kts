38e6d0fa5ffafc6346fda21a565fcc8972b0fb03
Maksim <maksvarlyga@gmail.com>
2025-12-11 17:50:02 +0300

    feat: added JUnit5 with jacoco

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(deps.plugins.kotlin.jvm)
     alias(deps.plugins.compose)
     alias(deps.plugins.compose.plugin)
+    id("jacoco")
 }
 
 group = "io.github.vvmaksim"
@@ -30,4 +31,31 @@ dependencies {
     implementation(deps.chesslib)
     implementation(deps.apache.poi)
     implementation(deps.apache.poi.ooxml)
+
+    testImplementation(deps.junit.jupiter.api)
+    testRuntimeOnly(deps.junit.jupiter.engine)
+    testImplementation(deps.junit.jupiter.params)
+    testImplementation(deps.junit.platform.launcher)
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
+
+jacoco {
+    toolVersion = "0.8.14"
+}
+
+tasks.jacocoTestReport {
+    dependsOn(tasks.test)
+    reports {
+        xml.required.set(false)
+        csv.required.set(false)
+        html.required.set(true)
+        html.outputLocation.set(layout.buildDirectory.dir("reports/jacoco/test/html"))
+    }
+}
+
+tasks.test {
+    finalizedBy(tasks.jacocoTestReport)
 }
