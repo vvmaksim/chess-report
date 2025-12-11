4e7c766788aa47424922fa4c5c82c42408505880
Maksim <maksvarlyga@gmail.com>
2025-12-11 17:50:37 +0300

    feat(test): added ChessManagerTest.kt

---
+++ src/test/kotlin/io/github/vvmaksim/model/ChessManagerTest.kt
@@ -0,0 +1,44 @@
+package io.github.vvmaksim.model
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class ChessManagerTest {
+    @Test
+    fun `getMoves should return correct moves v1`() {
+        val pgnString = "1. e4 e5"
+        val expectedMoves = listOf(Triple(1, "e4", "e5"))
+        val moves = ChessManager.getMoves(pgnString)
+        assertEquals(expectedMoves, moves)
+    }
+
+    @Test
+    fun `getMoves should return correct moves v2`() {
+        val pgnString = "1. e4 c5 2. Nc3 d6 3. Nf3 Nf6 4. g3"
+        val expectedMoves =
+            listOf(
+                Triple(1, "e4", "c5"),
+                Triple(2, "Nc3", "d6"),
+                Triple(3, "Nf3", "Nf6"),
+                Triple(4, "g3", null),
+            )
+        val moves = ChessManager.getMoves(pgnString)
+        assertEquals(expectedMoves, moves)
+    }
+
+    @Test
+    fun `getMoves should return empty moves list`() {
+        val pgnString = ""
+        val expectedMoves = emptyList<Triple<Int, String, String?>>()
+        val moves = ChessManager.getMoves(pgnString)
+        assertEquals(expectedMoves, moves)
+    }
+
+    @Test
+    fun `getMoves should return empty moves list because moves in bot correct`() {
+        val pgnString = "1. e4 e9"
+        val expectedMoves = emptyList<Triple<Int, String, String?>>()
+        val moves = ChessManager.getMoves(pgnString)
+        assertEquals(expectedMoves, moves)
+    }
+}
